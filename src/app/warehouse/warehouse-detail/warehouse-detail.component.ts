3ba53dd15c22ff6892ae1cbf8f3e822d40411a2b
saif <saif@tradexa.co.in>
2026-04-25 00:16:08 +0530

    warehouse management project setup

---
+++ src/app/warehouse/warehouse-detail/warehouse-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './warehouse-detail.component.html',
+  styleUrl: './warehouse-detail.component.scss'
+})
+export class WarehouseDetailComponent {
+
+}
